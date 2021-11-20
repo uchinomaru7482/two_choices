fbd7bfa025d5dd9c3a0ae3a20e9cce94d4bf5f42
Uchinomaru <bravecred@gmail.com>
2021-11-21 01:38:26 +0900

    次の質問へ行く為のタイマ追加

--- two_choices_client/pages/index.vue
+++ two_choices_client/pages/index.vue
@@ -4,61 +4,60 @@
     {{ question.getFirstAnswer() }}<br>
     {{ question.getSecondAnswer() }}<br> -->
     <span class="content">
-      <span>
-        <span class="question">あなたはどっち派？</span>
-        <v-row>
-          <v-col
-            class="percent"
-            cols="6"
+      <p class="next-count">{{ nextCount }}</p>
+      <p class="question">あなたはどっち派？</p>
+      <v-row>
+        <v-col
+          class="percent"
+          cols="6"
+        >
+          20%(20票)
+        </v-col>
+        <v-col
+          class="percent"
+          cols="6"
+        >
+          80%(80票)
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          class="answer"
+          cols="6"
+        >
+          いぬ
+        </v-col>
+        <v-col
+          class="answer"
+          cols="6"
+        >
+          ねこ
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          class="answer"
+          cols="6"
+        >
+          <img 
+            src="@/static/images/answer_image/dog.png"
+            class="ans-img"
+            alt="回答1"
+            @click="selectFirst()"
           >
-            20%(20票)
-          </v-col>
-          <v-col
-            class="percent"
-            cols="6"
+        </v-col>
+        <v-col
+          class="answer"
+          cols="6"
+        >
+          <img 
+            src="@/static/images/answer_image/cat.png"
+            class="ans-img"
+            alt="回答2"
+            @click="selectSecond()"
           >
-            80%(80票)
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col
-            class="answer"
-            cols="6"
-          >
-            いぬ
-          </v-col>
-          <v-col
-            class="answer"
-            cols="6"
-          >
-            ねこ
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col
-            class="answer"
-            cols="6"
-          >
-            <img 
-              src="@/static/images/answer_image/dog.png"
-              class="ans-img"
-              alt="回答1"
-              @click="selectFirst()"
-            >
-          </v-col>
-          <v-col
-            class="answer"
-            cols="6"
-          >
-            <img 
-              src="@/static/images/answer_image/cat.png"
-              class="ans-img"
-              alt="回答2"
-              @click="selectSecond()"
-            >
-          </v-col>
-        </v-row>
-      </span>
+        </v-col>
+      </v-row>
     </span>
   </div>
 </template>
@@ -69,7 +68,8 @@ import * as userQuestionPb from '@/generated/user_question_pb'
 import * as userQuestionService from '@/services/user_question'
 
 const CLASSNAME = '-visible'
-const AFTER_PERCENT_COLOR = 'rgba(255,255,255,100)'
+const OPAQUE_WHITE_COLOR = 'rgba(255,255,255,100)'
+const OPAQUE_GRAY_COLOR = 'rgba(200,200,200,100)'
 
 @Component
 export default class Home extends Vue {
@@ -78,8 +78,11 @@ export default class Home extends Vue {
   private selected = 0
   private percent = 50
   private afterPercent = 50
-  private timer: NodeJS.Timeout | null = null
-  private percentColor = 'rgba(255,255,255,0)'
+  private resultTimer: NodeJS.Timeout | null = null
+  private whiteColor = 'rgba(255,255,255,0)'
+  private grayColor = 'rgba(200,200,200,0)'
+  private nextCount = 3
+  private nextTimer: NodeJS.Timeout | null = null
 
   private async created () {
     try {
@@ -95,24 +98,37 @@ export default class Home extends Vue {
   private get style (): any {
     return {
       '--percent': String(this.percent) + '%',
-      '--percent-color': this.percentColor
+      '--white': this.whiteColor,
+      '--gray': this.grayColor
     }
   }
 
   @Watch('afterPercent')
   private watchPercent () {
-    this.timer = setInterval(this.updatePercent, 10);
+    this.resultTimer = setInterval(this.dispResult, 10);
   }
 
-  private updatePercent () {
+  // 結果表示
+  private dispResult () {
     if (this.percent === this.afterPercent) {
-      clearInterval(this.timer as NodeJS.Timeout)
-      this.percentColor = AFTER_PERCENT_COLOR
+      clearInterval(this.resultTimer as NodeJS.Timeout)
+      this.whiteColor = OPAQUE_WHITE_COLOR
+      this.grayColor = OPAQUE_GRAY_COLOR
+      this.nextTimer = setInterval(this.nextQuestion, 1000);
     } else if (this.percent > this.afterPercent) {
-      this.percent = this.percent - 0.5
+      this.percent -= 0.5
     } else if (this.percent < this.afterPercent) {
-      this.percent = this.percent + 0.5
+      this.percent += 0.5
     }
+  }
+
+  // 3秒後に次の質問へ行く為のタイマ
+  private nextQuestion () {
+    if (this.nextCount <= 0) {
+      clearInterval(this.nextTimer as NodeJS.Timeout)
+      location.reload()
+    }
+    this.nextCount -= 1
   }
 
   private selectFirst () {
